f0786593cb1e85696cf8c717e587a24d75af05d2
Alessio Caporali <capox95@gmail.com>
2018-01-28 20:14:31 +0100

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,24 @@
+
+#ifndef functions_INCLUDE
+#define functions_INCLUDE
+#include "stdafx.h"
+#include "opencv2\opencv.hpp"
+#include "iostream"
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include "functions.h"
+
+using namespace cv;
+using namespace std;
+
+
+void drawing_box(Mat dst, vector<Point> points);
+MatND histogram(Mat src);
+Mat binarization(Mat src, int threshold);
+vector<float> rotation_lines(vector<Vec4i> r_lines, float rotation[], Mat src);
+Mat rotation_image(Mat src, float angle_rotation);
+tuple <vector<Vec4i>, float> barcode_orientation(Mat src);
+int counter_tickness_bars(Mat img, vector<float> px);
+
+#endif
