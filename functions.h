deb29bebb5832a9ba97228e7721a077429d013cb
Alessio Caporali <capox95@gmail.com>
2018-01-29 19:29:52 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -16,9 +16,11 @@ using namespace std;
 void drawing_box(Mat dst, vector<Point> points);
 MatND histogram(Mat src);
 Mat binarization(Mat src, int threshold);
-vector<float> rotation_lines(vector<Vec4i> r_lines, float rotation[], Mat src);
+vector<Vec4i> rotation_lines(vector<Vec4i> r_lines, float angle, Mat src);
 Mat rotation_image(Mat src, float angle_rotation);
 tuple <vector<Vec4i>, float> barcode_orientation(Mat src);
 int counter_tickness_bars(Mat img, vector<float> px);
+vector <Vec4i> gap(vector<Vec4i> r_lines);
+vector<float> corners_detector(vector<Vec4i> r_lines);
 
 #endif
