9c50e23d07a6eb62d05c35ef4468aeb16574a0fa
Alessio Caporali <capox95@gmail.com>
2018-02-03 16:07:17 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -14,15 +14,16 @@ using namespace std;
 
 void drawing_box(Mat dst, vector<Point> points);
 Mat rotation_image(Mat src, float angle_rotation);
-tuple <vector<Vec4i>, float> barcode_orientation(Mat src);
+tuple <vector<Vec4i>, float> barcode_orientation(Mat src, bool *flag);
 int counter_tickness_bars(Mat img, vector<float> px);
 vector <Vec4i> gap(vector<Vec4i> r_lines, int max_gap);
 vector<float> FirstLastDetector(vector<Vec4i> r_lines);
 Mat clahe(Mat bgr_image);
-vector <Vec4i> vertical_gap(vector<Vec4i> r2_lines, int max_vertical_gap);
+vector <Vec4i> vertical_gap(vector<Vec4i> r2_lines, Mat src);
 int clahe_detector(Mat src);
 void plot_histogram(Mat Hist, int histSize);
 vector<float> Harris(Mat src, vector<Point> roi, int *height);
 void scan_images(Mat src, vector<Point> harris_points);
 
 #endif
+#pragma once
